a10d2180b76ffc4817c7a894249fe4b1a7b60f26
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2026-01-18 23:37:31 +0200

    remove uneeded folders

--- Clinic.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Clinic.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
